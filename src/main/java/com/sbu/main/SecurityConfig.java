298f34e438618110c5f30631331300fb94b70a82
Nicholas <Nicholas.Genco@stonybrook.edu>
2017-04-22 16:04:13 -0400

    Security Config

---
+++ src/main/java/com/sbu/main/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.sbu.main;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Properties;
+
+/**
+ * Created by silda05 on 11/7/16.
+ */
+@CrossOrigin
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+
+                //Protects all requests from unauthorized users
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/create_user").permitAll()
+                .antMatchers("/admin").permitAll()
+                .anyRequest().authenticated()
+                
+
+                //Allows HTTP Basic auth
+                .and().httpBasic()
+
+
+                .and().headers()
+                .and().logout().disable()
+                //
+                .csrf().disable();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(inMemoryUserDetailsManager());
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        final Properties users = new Properties();
+        //add whatever other user you need
+        users.put("admin", "pass,ROLE_ADMIN,enabled");
+        users.put("employee","pass,ROLE_EMPLOYEE,enabled");
+        users.put("customer","pass,ROLE_CUSTOMER,enabled");
+
+        return new InMemoryUserDetailsManager(users);
+    }
+
+}
